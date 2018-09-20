d3b4603caeae81d0cefedf42f6dc98ea1472d4a2
Julien Marquet <jmarquet@z2r3p3.le-101.fr>
2018-09-20 15:08:30 +0200

    ft_get_env

--- ft_getenv.c
+++ ft_getenv.c
@@ -1,27 +1,42 @@
 /* ************************************************************************** */
 /*                                                          LE - /            */
 /*                                                              /             */
-/*   ft_strswapfree.c                                 .::    .:/ .      .::   */
+/*   ft_getenv.c                                      .::    .:/ .      .::   */
 /*                                                 +:+:+   +:    +:  +:+:+    */
 /*   By: jmarquet <jmarquet@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
-/*   Created: 2018/04/24 11:04:17 by jmarquet     #+#   ##    ##    #+#       */
-/*   Updated: 2018/09/20 15:05:51 by jmarquet    ###    #+. /#+    ###.fr     */
+/*   Created: 2018/09/19 16:12:40 by jmarquet     #+#   ##    ##    #+#       */
+/*   Updated: 2018/09/19 16:55:48 by jmarquet    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdlib.h>
 
-int		ft_strswapfree(char **buffer, char *str)
+char	*ft_getenv(const char *name, char **env)
 {
-	char	*tmp;
+	size_t	i;
+	size_t	j;
 
-	if (buffer == NULL || *buffer == NULL || str == NULL)
-		return (0);
-	tmp = *buffer;
-	*buffer = str;
-	free(tmp);
-	return (1);
+	i = 0;
+
+	while (env[i] != NULL)
+	{
+		ft_printf("%s\n", env[i]);
+		j = 0;
+		while (name[j] == env[i][j])
+		{
+			if (env[i][j] == '\0')
+				continue ;
+			else if (name[j] == '\0')
+			{
+				if (env[i][j] == '=')
+					return (ft_strdup(&(env[i][j + 1])));
+				continue ;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (NULL);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jmarquet <jmarquet@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2017/11/09 15:44:12 by jmarquet     #+#   ##    ##    #+#       */
-/*   Updated: 2018/09/15 17:25:39 by jmarquet    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/09/19 16:13:38 by jmarquet    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -121,4 +121,5 @@ size_t			ft_numlen_ll(long long num);
 size_t			ft_numlen_i(int num);
 size_t			ft_numlen_c(char num);
 int				ft_printf(const char *format, ...);
+char			*ft_getenv(const char *name, char **env);
 #endif
