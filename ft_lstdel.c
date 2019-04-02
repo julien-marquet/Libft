ca942446d6990980dce68aaa7d4851f5bd5ee76c
Julien Marquet <jmarquet@z2r1p1.le-101.fr>
2019-04-02 22:04:15 +0200

    no more segfault in lstdel

--- ft_lstdel.c
+++ ft_lstdel.c
@@ -6,7 +6,7 @@
 /*   By: jmarquet <jmarquet@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2017/12/07 18:28:39 by jmarquet     #+#   ##    ##    #+#       */
-/*   Updated: 2019/02/08 23:05:14 by jmarquet    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/04/02 21:44:15 by jmarquet    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -15,14 +15,16 @@
 
 void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
 {
+	t_list	*to_free;
 	if (alst)
 	{
 		while ((*alst))
 		{
 			if ((*alst)->content)
 				del((*alst)->content, (*alst)->content_size);
-			free(*alst);
+			to_free = *alst;
 			*alst = (*alst)->next;
+			free(to_free);
 		}
 	}
 }
