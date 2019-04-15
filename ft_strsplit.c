06e7ce828275891965f2efeff6ba4229470721b4
legrivel <legrivel@student.le-101.fr>
2019-04-15 22:49:27 +0200

    Added my own strsplit

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -3,108 +3,128 @@
 /*                                                              /             */
 /*   ft_strsplit.c                                    .::    .:/ .      .::   */
 /*                                                 +:+:+   +:    +:  +:+:+    */
-/*   By: jmarquet <jmarquet@student.le-101.fr>      +:+   +:    +:    +:+     */
+/*   By: legrivel <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
-/*   Created: 2017/12/07 18:28:51 by jmarquet     #+#   ##    ##    #+#       */
-/*   Updated: 2018/09/25 17:26:19 by jmarquet    ###    #+. /#+    ###.fr     */
+/*   Created: 2017/11/26 01:54:21 by legrivel     #+#   ##    ##    #+#       */
+/*   Updated: 2017/11/26 01:54:21 by legrivel    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
-#include <stdlib.h>
+#include "libft.h"
 
-static unsigned int		ft_count_words(char const *s, char c)
+static size_t	get_tab_length(char const *s, char c)
 {
-	unsigned int	i;
-	unsigned int	cpt;
-	int				in_a_word;
+	size_t	i;
+	size_t	j;
 
-	i = 0;
-	cpt = 0;
-	in_a_word = s[0] != '\0' && s[0] != c ? 1 : 0;
-	while (s[i] != '\0')
-	{
-		if (in_a_word && s[i] == c)
-		{
-			cpt++;
-			in_a_word = 0;
-		}
-		else if (!in_a_word && s[i] != c)
-			in_a_word = 1;
-		if (in_a_word && s[i + 1] == '\0')
-			cpt++;
-		i++;
-	}
-	return (cpt);
-}
-
-static unsigned int		ft_split_strlen(char const *s, char c)
-{
-	unsigned int	i;
-
-	i = 0;
-	while (s[i] != '\0' && s[i] != c)
-		i++;
-	return (i);
-}
-
-static void				ft_free_allocated(char **array, unsigned int index)
-{
-	long	i;
-
-	i = (long)index;
-	i--;
-	while (i >= 0)
-		free(array[i--]);
-	free(array);
-}
-
-static void				ft_fill_string(char **str, char const **src, char c)
-{
-	unsigned int	i;
-	unsigned int	j;
-
+	if (s == NULL)
+		return (0);
 	i = 0;
 	j = 0;
-	while ((*src)[j] != '\0' && (*src)[j] == c)
-		j++;
-	while ((*src)[j] != '\0' && (*src)[j] != c)
+	while (s[i])
 	{
-		(*str)[i] = (*src)[j];
-		i++;
-		j++;
+		while (s[i] == c && s[i])
+			i += 1;
+		while (s[i] != c && s[i])
+			i += 1;
+		j += 1;
 	}
-	(*str)[i] = '\0';
-	while ((*src)[j] != '\0' && (*src)[j] == c)
-	{
-		i++;
-		j++;
-	}
-	*src = (*src + j);
+	return (j);
 }
 
-char					**ft_strsplit(char const *s, char c)
+static void		*allocate_tab(char const *s, char c, char **tab)
 {
-	char			**array;
-	unsigned int	array_len;
-	unsigned int	array_index;
+	size_t	i;
+	size_t	j;
+	size_t	k;
 
-	if (!s)
-		return (NULL);
-	array_len = ft_count_words(s, c);
-	if (!(array = (char**)malloc(sizeof(char *) * (array_len + 1))))
-		return (NULL);
-	array_index = -1;
-	while (++array_index < array_len)
+	i = 0;
+	k = 0;
+	while (s[i])
 	{
-		if (!(array[array_index] = (char *)malloc(sizeof(char)
-			* (ft_split_strlen(s, c) + 1))))
-		{
-			ft_free_allocated(array, array_index);
+		j = 0;
+		while (s[i] && s[i] == c)
+			i += 1;
+		while (s[i] && s[i++] != c)
+			j += 1;
+		if (j > 0 && (tab[k++] = malloc(j + 1)) == NULL)
 			return (NULL);
-		}
-		ft_fill_string(&array[array_index], &s, c);
 	}
-	array[array_len] = NULL;
-	return (array);
+	if (s[0] == '\0')
+	{
+		if ((tab[k] = malloc(1)) == NULL)
+			return (NULL);
+		tab[k++][0] = '\0';
+	}
+	tab[k] = NULL;
+	return (tab[0]);
+}
+
+static int		is_cutable(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s++ != c)
+			return (1);
+	}
+	return (0);
+}
+
+static char		**manage_short_tab(char **tab, char const *s, char c)
+{
+	size_t	i;
+	size_t	j;
+
+	if (!is_cutable((char *)s, c))
+	{
+		if ((tab = malloc(sizeof(char *))) == NULL)
+			return (NULL);
+		tab[0] = NULL;
+		return (tab);
+	}
+	i = 0;
+	while (s[i] == c)
+		i += 1;
+	j = 0;
+	while (s[i] != c && s[i])
+	{
+		i += 1;
+		j += 1;
+	}
+	if ((tab = malloc(sizeof(char *) * 2)) == NULL ||
+			(tab[0] = malloc(j + 1)) == NULL)
+		return (NULL);
+	ft_strcpy(tab[0], &s[i - j]);
+	tab[1] = NULL;
+	return (tab);
+}
+
+char			**ft_strsplit(char const *s, char c)
+{
+	size_t	i;
+	size_t	j;
+	size_t	k;
+	char	**tab;
+
+	tab = NULL;
+	if (get_tab_length(s, c) == 1)
+		return (manage_short_tab(tab, s, c));
+	if (s == NULL || (tab = malloc(sizeof(char *) * (get_tab_length(s, c) + 1)\
+)) == NULL || (allocate_tab(s, c, tab) == NULL))
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s[i])
+	{
+		while (s[i] == c && s[i])
+			i += 1;
+		k = 0;
+		while (s[i] != c && s[i])
+			tab[j][k++] = s[i++];
+		if (k > 0)
+			tab[j++][k] = '\0';
+	}
+	tab[j] = NULL;
+	return (tab);
 }
